050047294af51d6daca816081001d696eb8c1ec4
martin sahadad <martin.sahadad@elev.ga.ntig.se>
2024-08-30 10:52:14 +0200

    potatis macka

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,4 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿Console.WriteLine("Användarnamn:");
+String name = Console.ReadLine();
+
+if (name == "micke")
+{
+Console.WriteLine("Inloggad! COYS!");
+}
+else
+{
+Console.WriteLine("GÅ GÖR EN MACKA TILL MIG :)!");
+}
 
 Console.ReadLine();
